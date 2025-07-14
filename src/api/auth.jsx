6fa232ba24d2e974d9aa109cdf23595f041bd68a
hasol2117 <hasol211704@gmail.com>
2025-07-14 12:18:23 +0900

    chore: gitignore 편집

--- src/api/auth.jsx
+++
@@ -1,6 +0,0 @@
-import { api } from "./http";
-
-//공지사항
-export const noticeDetail = (noticeIdx) =>
-  api.get(`/notice/detail/${noticeIdx}`);
-export const noticeList = () => api.get("/notice/list");
