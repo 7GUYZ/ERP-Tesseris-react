5a15ddf75667822e671c0ddfefe44e6b53c66160
jipi <okj1227@gmail.com>
2025-07-22 16:40:11 +0900

    25.07.22 충전 헤더 제거

--- src/components/layout/jihun/charge/ChargeLayout.jsx
+++ src/components/layout/jihun/charge/ChargeLayout.jsx
@@ -5,10 +5,10 @@ const ChargeLayout = ({ children }) => {
   return (
     <div className="charge-layout">
       <div className="charge-container">
-        <div className="charge-header">
+        {/* <div className="charge-header">
           <h1 className="charge-title">CM 충전</h1>
-        </div>
-        
+          <p className="charge-subtitle">안전하고 빠른 CM 충전 서비스</p>
+        </div> */}
         <div className="charge-content">
           {children}
         </div>
