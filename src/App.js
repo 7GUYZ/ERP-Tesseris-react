bba16bd57847b82d2926b3ae8b11663aa636f5bb
hellow <ksc931007@gmail.com>
2025-07-23 13:23:07 +0900

    test3

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <ToastProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.REACT_APP_BASENAME || "/react"}>
         <Routes>
           <Route path='/' element={<LoginPage />} />
           <Route path='/TestSignUp' element={<TestSignUp />} /> {/* 임시 회원가입 페이지 */}
